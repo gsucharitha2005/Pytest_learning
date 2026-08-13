fc409aa434c4eda3853bfb60fd72c24c0a95c95f
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-08-13 18:48:36 +0530

    writting unit test for multiplication and even numbers

---
+++ test_even_number.py
@@ -0,0 +1,11 @@
+from even_number import is_even
+def test_even_number():
+	assert is_even(4)==True
+def test_even_number():
+	assert is_even(5)==False
+def test_even_number():
+	assert is_even(0)==True
+
+
+
+	
---
+++ test_multiply.py
@@ -0,0 +1,7 @@
+from multiply import multiply
+def test_multiply():
+	assert multiply(2,8)==16
+def test_multiply():
+	assert multiply(-2,-6)==12
+def test_multiply():
+	assert multiply(4,0)==0
